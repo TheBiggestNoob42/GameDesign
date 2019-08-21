2c7bfd1509ee39794f6ea9f811a659c790dce1d0
Kevin Sony <kevinsony2003@gmail.com>
2019-08-21 16:38:39 -0500

    Created an animation loop

--- panda_actor.py
+++ panda_actor.py
@@ -3,6 +3,8 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 
 class PandaApp(ShowBase):
@@ -28,6 +30,28 @@ class PandaApp(ShowBase):
         self.pandaActor.reparentTo(self.render)
         # Loop its animation
         self.pandaActor.loop("walk-boi")
+
+        # Create the four loop intervals needed for the panda to walk back and forth
+        pandaPosInterval1 = self.pandaActor.posInterval(13,
+                                                        Point3(0, -10, 0),
+                                                        startPos=Point3(0, 10, 0))
+        pandaPosInterval2 = self.pandaActor.posInterval(13,
+                                                        Point3(0, 10, 0),
+                                                        startPos=Point3(0, -10, 0))
+        pandaHprInterval1 = self.pandaActor.hprInterval(3,
+                                                        Point3(180, 0, 0),
+                                                        startHpr=Point3(0, 0, 0))
+        pandaHprInterval2 = self.pandaActor.hprInterval(3,
+                                                        Point3(0, 0, 0),
+                                                        startHpr=Point3(180, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(pandaPosInterval1,
+                                  pandaHprInterval1,
+                                  pandaPosInterval2,
+                                  pandaHprInterval2,
+                                  name="pandaPace")
+        self.pandaPace.loop()
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
