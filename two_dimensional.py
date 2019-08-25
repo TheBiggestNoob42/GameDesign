6ab72009daa590f4c8fc29730f4ed39ef72e2ff0
Kevin Sony <kevinsony2003@gmail.com>
2019-08-25 18:46:51 -0500

    Trying out panda3d asteriod thinghy

---
+++ two_dimensional.py
@@ -0,0 +1,15 @@
+from math import pi, sin, cos
+
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
+
+
+class TwoD(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        
